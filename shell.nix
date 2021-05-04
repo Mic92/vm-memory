2f143225ee31bfb531ed281d521bba1a4722a6e8
Pogobanane <aenderboy@gmx.de>
2021-05-04 13:51:52 +0000

    add dev infrastructure

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell {
+  #RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+  nativeBuildInputs = [
+    pkgs.niv
+    pkgs.rls
+    pkgs.rust-analyzer
+    pkgs.rustfmt
+    pkgs.just
+    pkgs.clippy
+    pkgs.rustfmt
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.cargo-watch
+    pkgs.cargo-deny
+    pkgs.pre-commit
+    pkgs.git # needed for pre-commit install
+    #pythonEnv
+
+    pkgs.qemu_kvm
+    pkgs.gdb
+    pkgs.tmux # needed for integration test
+  ]; #++ vmsh.nativeBuildInputs;
+  #buildInputs = vmsh.buildInputs;
+  shellHook = ''
+    pre-commit install
+  '';
+}
